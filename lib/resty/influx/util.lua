2786fc6383e7066d98f21edc8a3c0cab3331c36f
Robert Paprocki <robert@cryptobells.com>
2017-12-27 08:43:02 -0800

    Define resty http query params as a table
    This resolves compatability issues among different versions of
    lua-resty-http that broke the URI parser for some syntaxes.

--- lib/resty/influx/util.lua
+++ lib/resty/influx/util.lua
@@ -54,7 +54,7 @@ function _M.write_http(msg, params)
 	local res, err = client:request_uri(
 		path,
 		{
-			query      = str_fmt("db=%s&precision=%s", params.db, params.precision),
+			query      = { db = params.db, precision = params.precision },
 			method     = method,
 			headers    = headers,
 			body       = msg,
